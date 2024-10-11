28513279de2251a3527e6c3d5feaf59fab396ab7
JaeYoung <kimjaeyoung650@gmail.com>
2024-10-11 13:12:43 +0900

    컴포넌트 수정: common components Button

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -1,76 +1,94 @@
 import styled, { css } from "styled-components";
 
-const BORDER = {
-  pill: css`
-    border-radius: 9999px;
-  `,
-}
-
-const VARIANTS = {
-  success: css`
-    --button-bg-color: #28a745;
-    --button-hover-bg-color: #218838;
-  `,
-  danger: css`
-    --button-bg-color: #dc3545;
-    --button-hover-bg-color: #c82333;
-  `,
-  warning: css`
-    --button-bg-color: #ffc107;
-    --button-hover-bg-color: #e0a800;
-  `,
-};
-
-const SIZES = {
-  sm: css`
-    --button-font-size: 0.875rem;
-    --button-padding: 8px 12px;
-  `,
-  md: css`
-    --button-font-size: 1rem;
-    --button-padding: 12px 24px;
-  `,
-  lg: css`
-    --button-font-size: 1.25rem;
-    --button-padding: 16px 20px;
-  `
-};
-
-
-function Button({ lable, size, border, variant }) {
-  
-  const sizeStyle = SIZES[size];
-  const borderStyle = BORDER[border];
-  const variantStyle = VARIANTS[variant];
-
-  return (
-    <CustomButton 
-      className="text-white rounded"
-      sizeStyle={sizeStyle}
-      borderStyle={borderStyle}
-      variantStyle={variantStyle}
-    >
-      {lable}
-    </CustomButton>
-  );
-}
-
-const CustomButton = styled.button
-`
+// 버튼 기본
+const StyledButton = styled.button`
   ${(p) => p.sizeStyle}
   ${(p) => p.borderStyle}
-  ${(p) => p.variantStyle}
+  ${(p) => p.colorStyle}
 
   margin: 0;
   border: none;
   cursor: pointer;
   font-family: "Noto Sans KR", sans-serif;
-  padding: var(--button-padding, 12px 16px);
-  background: var(--button-bg-color, #0d6efd);
+  color: var(--button-color, rgb(255 255 255));
+  font-size: var(--button-font-size, 1rem);
+  font-weight: var(--button-font-weight, 600);
+  padding: var(--button-padding, 10px 20px);
+  border-radius: var(--button-radius, 5px);
+  background: var(--button-bg-color, rgb(59 130 246));
 
   &:active, &:hover, &:focus {
-    background: var(--button-hover-bg-color, #025ce2);
+    background: var(--button-hover-bg-color, rgb(29 78 216));
   }
 `;
+// 버튼 테두리
+const BORDER = {
+  pill: css`
+    --button-radius: 999px;
+  `,
+}
+// 버튼 색상
+const COLOR = {
+  red: css`
+    --button-bg-color: rgb(239 68 68);
+    --button-hover-bg-color: rgb(185 28 28);
+  `,
+  green: css`
+    --button-bg-color: rgb(34 197 94);
+    --button-hover-bg-color: rgb(21 128 61);
+  `,
+  yellow: css`
+    --button-bg-color: rgb(234 179 8);
+    --button-hover-bg-color: rgb(161 98 7);
+  `,
+  indigo: css`
+    --button-bg-color: rgb(99 102 241);
+    --button-hover-bg-color: rgb(67 56 202);
+  `,
+  violet: css`
+    --button-bg-color: rgb(139 92 246);
+    --button-hover-bg-color: rgb(109 40 217);
+  `,
+  gray: css`
+    --button-bg-color: rgb(107 114 128);
+    --button-hover-bg-color: rgb(55 65 81);
+  `,
+};
+// 버튼 사이즈
+const SIZES = {
+  sm: css`
+    --button-font-size: 0.75rem;
+    --button-font-weight: 500;
+    --button-padding: 5px 10px;
+  `,
+  md: css`
+    --button-font-size: 1rem;
+    --button-font-weight: 600;
+    --button-padding: 10px 20px;
+  `,
+  lg: css`
+    --button-font-size: 1.25rem;
+    --button-font-weight: 700;
+    --button-padding: 15px 30px;
+  `
+};
+
+function Button(props) {
+  const { children, size, color, border } = props;
+
+  const sizeStyle = SIZES[size];
+  const colorStyle = COLOR[color];
+  const borderStyle = BORDER[border];
+
+  return (
+    <StyledButton 
+      sizeStyle={sizeStyle}
+      colorStyle={colorStyle}
+      borderStyle={borderStyle}
+    >
+      {children}
+    </StyledButton>
+  );
+}
 
 export default Button;
