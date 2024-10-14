2db3eb7e3c0c544565d05ee0bf61dd2646237774
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-10-14 22:42:38 +0900

    FIX: conditional structure and improve Drawer component logic

--- src/components/Common/Drawer.jsx
+++ src/components/Common/Drawer.jsx
@@ -1,12 +1,13 @@
-import styled, { css } from "styled-components";
+import React, { useState, useEffect } from "react";
+
+import styled from "styled-components";
 import Button from "./Button";
-import { Children } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const StyledDrawer = styled.div`
   width: ${(p) => p.width}px;
-  height: 100vh;
   color: #FFFFFF;
-  font-size: 24px;
   font-weight: 600;
   padding: 0px 12px;
   background-color: rgb(59 130 246);
@@ -14,19 +15,34 @@ const StyledDrawer = styled.div`
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
 `;
 
-function Drawer(props) {
-  const { children, open, active } = props;
+function Drawer({ children }) {
+  const [open, setOpen] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  let width;
+  // 창 크기에 맞춰 동적으로 너비를 설정
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
 
-  {open ? width = 240 : width = 70};
+  // 창 크기 변경 이벤트 리스너 등록
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth);
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  }, []);
+
+  // Drawer 너비 설정: 창 너비에 비례하도록 설정
+  const drawerWidth = open ? Math.max(windowWidth * 0.18, 250) : 70; // 창 너비의 30% 또는 최대 300px
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   return (
-    <StyledDrawer width={width}>
-      <div className="py-3 flex justify-end">
-        <Button size="sm" color="indigo" onClick={active}>Open & Close</Button>
-      </div>
-      {children}
+    <StyledDrawer width={drawerWidth}>
+      <Button size="sm" color="transparent" onClick={toggleDrawer}>
+        <FontAwesomeIcon icon={faBars} />
+      </Button>
+      {open && children}
     </StyledDrawer>
   );
 }
