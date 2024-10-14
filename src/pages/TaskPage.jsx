a7bedc39da7dcbe488b48d67af564dcc719dd84a
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-10-14 22:47:21 +0900

    FEAT: add TaskPage

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Drawer from '../components/Common/Drawer';
 import TaskDrawer from '../components/Task/TaskDrawer';
 import TaskDashboard from '../components/Task/TaskDashboard';
+import TaskModal from '../components/Task/TaskModal';
 
 const TaskPage = () => {
   return (
@@ -10,6 +11,7 @@ const TaskPage = () => {
         <TaskDrawer /> {/* TaskDrawer가 Drawer의 너비에 맞춰짐 */}
       </Drawer>
       <TaskDashboard />
+      <TaskModal />
     </div>
   );
 };
