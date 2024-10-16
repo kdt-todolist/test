de98c7f53c3985b032b438922f2cc14ea4a4a8b8
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-10-16 22:42:16 +0900

    FEAT: auth provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TaskPage from './pages/TaskPage';
+import { AuthProvider } from './contexts/AuthContext';
 import { TaskProvider } from './contexts/TaskContext';
 
 function App() {
   return (
+    <AuthProvider>
     <TaskProvider>
       <TaskPage />
     </TaskProvider>
+    </AuthProvider>
   );
 }
 
