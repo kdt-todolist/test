aafeae31ef142400f36d6a2ecaf5bcea8a48295b
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-10-16 22:19:02 +0900

    FEAT: google auth login

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -3,6 +3,11 @@ import { FaGoogle } from "react-icons/fa";
 import Button from "../Common/Button";
 
 function LoginFrom(props) {
+  const handleGoogleLogin = () => {
+    // 구글 OAuth 요청을 백엔드로 보냄
+    window.location.href = 'http://localhost:1009/auth/google';
+  };
+
   return (
     <div className="grid gap-5">
       <h3 className="text-center text-2xl font-bold">소셜 로그인</h3>
@@ -10,6 +15,7 @@ function LoginFrom(props) {
         size="lg"
         color="gray"
         border="pill"
+        onClick={handleGoogleLogin} // 버튼 클릭 시 구글 로그인 요청
       >
         <FaGoogle style={{ width: '28px', height: '28px', marginRight: '10px' }}/>
         구글 로그인
