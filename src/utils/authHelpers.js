702c8561f79736c7e196c1a20a0622a19f3ec446
7evendays <68999618+CodingKirby@users.noreply.github.com>
2024-10-18 06:54:49 +0900

    FEAT: window confirm to ok

--- src/utils/authHelpers.js
+++ src/utils/authHelpers.js
@@ -1,7 +1,5 @@
 // 토큰과 관련된 유틸리티 함수를 관리하는 파일
 // authHelpers.js
-
-// 토큰에서 만료 시간을 추출하는 함수
 export const getTokenExpiration = (token) => {
     const payload = JSON.parse(atob(token.split('.')[1]));
     return payload.exp * 1000;
@@ -18,11 +16,7 @@ export const getTokenExpiration = (token) => {
 
   export const handleAuthError = (error) => {
     if (error.response?.status === 401) {
-      const loginConfirm = window.confirm('로그인 시간이 만료되었습니다. 다시 로그인하시겠습니까?');
-      if (loginConfirm) {
-        window.location.href = '/login'; // 로그인 페이지로 리다이렉트
-      }
-      return false;
+      window.ok('로그인 시간이 만료되었습니다. 다시 로그인해주세요.');
     }
     console.error('서버와의 통신 오류:', error);
     return true;
